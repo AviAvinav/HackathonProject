b6a4f55ba184174b016552c8a115e07100b7bf38
AviAvinav <54372449+AviAvinav@users.noreply.github.com>
2020-06-14 02:25:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,46 @@ def population_densities():
   os.open('populationDensities.py')
   window.destroy()
   exit()
+
+def search():
+  os.open('search.py')
+  window.destroy()
+  exit()
+
+def ChemVis():
+  os.open('chemScreen.py')
+  window.destroy()
+  exit()
+
+def Graphs():
+  os.open('graphs.py')
+  window.destroy()
+  exit()
+  
+def exiting():
+  window.destroy()
+  exit()
   
 #code for pics and name
 pic = tk.PhotoImage(file="images\\first.png")
 tk.Label(window, text="Hackathon Project", bg="black", fg="white", font="none").grid(row=0, column=0, sticky=W)
 
 #code for population buttons
-   #population densities
+
+#population densities
 tk.Button(window, text='Population Densities', width=6, command=population_densities).grid(row=1, column=0, sticky=W)
-   
+
+#Search
+tk.Button(window, text='Search', width=6, command=search).grid(row=2, column=0, sticky=W)
+
+#Chem Visualisations
+tk.Button(window, text='Chem Visualisations', width=6, command=ChemVis).grid(row=3, column=0, sticky=W)
+
+#Graphs
+tk.Button(window, text='Graphs', width=6, command=Graphs).grid(row=4, column=0, sticky=W)
+
+#Exit Button
+tk.Button(window, text='Exit', width=6, command=exiting).grid(row=5, column=0, sticky=W)
+
     
 window.mainloop()
