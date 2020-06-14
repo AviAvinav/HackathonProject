8c022dc981e51471426336e6e845189283eeb435
AviAvinav <54372449+AviAvinav@users.noreply.github.com>
2020-06-14 10:49:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,25 +32,26 @@ def exiting():
   exit()
   
 #code for pics and name
-pic = tk.PhotoImage(file="images\\first.png")
-tk.Label(window, text="Hackathon Project", bg="black", fg="white", font="none").grid(row=0, column=0, sticky=W)
+pic = tk.PhotoImage(file="images//First.jpg")
+tk.Label(window, bg="black", image=pic).grid(row=0, column=0, sticky=W)
+tk.Label(window, text="Hackathon Project", bg="black", fg="white", font="none").grid(row=1, column=0, sticky=W)
 
 #code for population buttons
 
 #population densities
-tk.Button(window, text='Population Densities', width=6, command=population_densities).grid(row=1, column=0, sticky=W)
+tk.Button(window, text='Population Densities', width=6, command=population_densities).grid(row=2, column=0, sticky=W)
 
 #Search
-tk.Button(window, text='Search', width=6, command=search).grid(row=2, column=0, sticky=W)
+tk.Button(window, text='Search', width=6, command=search).grid(row=3, column=0, sticky=W)
 
 #Chem Visualisations
-tk.Button(window, text='Chem Visualisations', width=6, command=ChemVis).grid(row=3, column=0, sticky=W)
+tk.Button(window, text='Chem Visualisations', width=6, command=ChemVis).grid(row=4, column=0, sticky=W)
 
 #Graphs
-tk.Button(window, text='Graphs', width=6, command=Graphs).grid(row=4, column=0, sticky=W)
+tk.Button(window, text='Graphs', width=6, command=Graphs).grid(row=5, column=0, sticky=W)
 
 #Exit Button
-tk.Button(window, text='Exit', width=6, command=exiting).grid(row=5, column=0, sticky=W)
+tk.Button(window, text='Exit', width=6, command=exiting).grid(row=6, column=0, sticky=W)
 
     
 window.mainloop()
